13055c72570deab0a1cda9616798587b955e50da
Andrew Martins <andrew_omartins@yahoo.com>
2021-12-03 12:24:53 -0500

    added some styling to the buttons and and am now going to manage state

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen  name="Home" component={HomeScreen} />
-        <Stack.Screen options={{headerShown: false }} name="Login" component={LoginScreen} />
+       <Stack.Screen options={{headerShown: false }} name="Login" component={LoginScreen} />
+       <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
